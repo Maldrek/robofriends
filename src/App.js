31a2d0369ada8e4f555f4c5706a7e1fc77faeaab
Maldrek <scott.salem5@gmail.com>
2019-10-28 16:11:47 -0500

    setting state between cardlist and searchfield to filter cards

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, Component} from 'react';
 import CardList from './components/CardList';
 import Searchbox from './components/Searchbox';
 import {robots} from './robots';
 
-const App = () => {
-    return(
-        <Fragment>
-            <h1 className='tc'>RoboFriends</h1>
-            <Searchbox />
-            <CardList robots={robots}/>
-        </Fragment>
-    )
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchField: ''
+        }
+    }
+
+    onSearchChange = event => {
+        this.setState({searchField: event.target.value});
+    }
+
+    render() {
+        const filtered = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+        });
+
+        return(
+            <Fragment>
+                <h1 className='tc'>RoboFriends</h1>
+                <Searchbox searchChange={this.onSearchChange}/>
+                <CardList robots={filtered}/>
+            </Fragment>
+        )
+    }
 }
 
 export default App;
--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Searchbox = () =>  {
+const Searchbox = ({searchField, searchChange}) =>  {
     return (
         <div className='pa2 tc'>
-            <input className='ba pa3 bg-lightest-blue' type='search' placeholder='Search Robots'></input>
+            <input 
+                className='ba pa3 bg-lightest-blue' 
+                type='search' 
+                placeholder='Search Robots'
+                onChange={searchChange}
+            />
         </div>
     )
 }
