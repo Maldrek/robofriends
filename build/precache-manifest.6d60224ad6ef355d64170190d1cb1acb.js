2c93b9150cb38149594c1f10345cd60e7f157734
Maldrek <scott.salem5@gmail.com>
2019-10-28 17:03:01 -0500

    created build

---
+++ build/precache-manifest.6d60224ad6ef355d64170190d1cb1acb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df7321f87d23d7064d4c67f91ce9ccf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72502260e5772456b464",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "de09d4d74b98415633a1",
+    "url": "/static/css/main.d0655c95.chunk.css"
+  },
+  {
+    "revision": "72502260e5772456b464",
+    "url": "/static/js/2.95e3fcdc.chunk.js"
+  },
+  {
+    "revision": "de09d4d74b98415633a1",
+    "url": "/static/js/main.ca8ee0c7.chunk.js"
+  },
+  {
+    "revision": "4857436c5671f36b0094",
+    "url": "/static/js/runtime-main.917c95b1.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
