81e9ecc5c76ba098f8b72be3cbe63047d3e5c769
Maldrek <scott.salem5@gmail.com>
2019-10-29 10:39:53 -0500

    Updates

---
+++ precache-manifest.cee7a1801cfdba137a9da268d8e4df26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eee0f0913456cf88af6790ed9f5eff62",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7f4d4b3d520ba3cfef14",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a260c071910d754b7ea2",
+    "url": "/robofriends/static/css/main.ed4b6493.chunk.css"
+  },
+  {
+    "revision": "7f4d4b3d520ba3cfef14",
+    "url": "/robofriends/static/js/2.3ff4439a.chunk.js"
+  },
+  {
+    "revision": "a260c071910d754b7ea2",
+    "url": "/robofriends/static/js/main.4ab7515f.chunk.js"
+  },
+  {
+    "revision": "cb519fe66fcbb61e7a5c",
+    "url": "/robofriends/static/js/runtime-main.0560b89d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
